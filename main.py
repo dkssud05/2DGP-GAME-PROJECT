9f2728988f061e51bc69b998e71ff2f07cfe2c57
dkssud05 <hyeonuson51@gmail.com>
2025-10-24 22:35:06 +0900

    게임 루프 생성, 이벤트 명령어 처리

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from pico2d import *
 
 open_canvas(800, 600)
-clear_canvas()
+
+running = True
+while running:
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+    delay(0.01)
+
+close_canvas()
