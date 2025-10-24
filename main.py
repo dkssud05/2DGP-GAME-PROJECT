32ffed1f456cffac0d0b33a6413efee083bc9693
dkssud05 <hyeonuson51@gmail.com>
2025-10-24 22:46:38 +0900

    character1 애니메이션 추가

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from pico2d import *
 
 open_canvas(800, 600)
 
-character1 = load_image('character1.motion/char1_Attack1.png')
+# Idle 애니메이션용 이미지 로드
+character1 = load_image('character1.motion/char1_Idle.png')
 
 char1_x = 400
 char1_y = 300
+frame = 0  # 애니메이션 프레임
 
 running = True
 while running:
 
     clear_canvas()
 
-    character1.draw(char1_x, char1_y)
+    # Idle 애니메이션 (10프레임)
+    character1.clip_draw(frame * 200, 0, 200, 200, char1_x, char1_y, 200, 200)
+
+    # 프레임 업데이트
+    frame = (frame + 1) % 8
 
     update_canvas()
 
@@ -25,6 +31,6 @@ while running:
             if event.key == SDLK_ESCAPE:
                 running = False
 
-    delay(0.01)
+    delay(0.05)
 
 close_canvas()
