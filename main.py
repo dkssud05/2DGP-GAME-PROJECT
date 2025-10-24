efd2ccaab2aa9b65fef6f035bb8c2c54ab7aadff
dkssud05 <hyeonuson51@gmail.com>
2025-10-24 22:37:57 +0900

    character1 화면에 띄우기

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from pico2d import *
 
 open_canvas(800, 600)
 
+character1 = load_image('character1.motion/char1_Attack1.png')
+
+char1_x = 400
+char1_y = 300
+
 running = True
 while running:
 
+    clear_canvas()
+
+    character1.draw(char1_x, char1_y)
+
+    update_canvas()
+
+    #이벤트 처리
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
