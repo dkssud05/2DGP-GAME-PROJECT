b60e57fab72858289ea18129b1b0f72b958d6470
dkssud05 <hyeonuson51@gmail.com>
2025-11-02 13:04:07 +0900

    boy 클래스 만들기

---
+++ boy.py
@@ -0,0 +1,42 @@
+from pico2d import *
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 300
+        self.frame = 0
+        self.dir = 0
+        self.image = load_image('character1.motion/char1_Idle.png')
+        self.is_jumping = False
+        self.jump_time = 0
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.dir * 5
+
+        if self.is_jumping:
+            if self.jump_time < 10:
+                self.y += 5
+            elif self.jump_time < 20:
+                self.y -= 5
+            else:
+                self.is_jumping = False
+                self.jump_time = 0
+            self.jump_time += 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 200, 0, 200, 200, self.x, self.y, 200, 200)
+
+    def handle_event(self, event):
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_LEFT:
+                self.dir = -1
+            elif event.key == SDLK_RIGHT:
+                self.dir = 1
+            elif event.key == SDLK_UP:
+                if not self.is_jumping:
+                    self.is_jumping = True
+                    self.jump_time = 0
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT or event.key == SDLK_RIGHT:
+                self.dir = 0
+
--- main.py
+++ main.py
@@ -1,41 +1,16 @@
 from pico2d import *
+from boy import Boy
 
 open_canvas(800, 600)
 
-# Idle 애니메이션용 이미지 로드
-character1 = load_image('character1.motion/char1_Idle.png')
-
-char1_x = 400
-char1_y = 300
-frame = 0
-dir_x = 0
-
-# 점프 관련 변수
-is_jumping = False
-jump_time = 0
+boy = Boy()
 
 running = True
 while running:
-
     clear_canvas()
 
-    character1.clip_draw(frame * 200, 0, 200, 200, char1_x, char1_y, 200, 200)
-
-    frame = (frame + 1) % 8
-
-    # 좌우 이동
-    char1_x += dir_x * 5
-
-    # 점프 처리 (간단한 위아래 이동)
-    if is_jumping:
-        if jump_time < 10:
-            char1_y += 5  # 위로 올라가기
-        elif jump_time < 20:
-            char1_y -= 5  # 아래로 내려가기
-        else:
-            is_jumping = False
-            jump_time = 0
-        jump_time += 1
+    boy.update()
+    boy.draw()
 
     update_canvas()
 
@@ -43,20 +18,10 @@ while running:
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-            elif event.key == SDLK_LEFT:
-                dir_x = -1
-            elif event.key == SDLK_RIGHT:
-                dir_x = 1
-            elif event.key == SDLK_UP:
-                if not is_jumping:
-                    is_jumping = True
-                    jump_time = 0
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_LEFT or event.key == SDLK_RIGHT:
-                dir_x = 0
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
 
     delay(0.05)
 
