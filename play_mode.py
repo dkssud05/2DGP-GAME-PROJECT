22d62c1f39e339099cadfdc88bebb4a2df38b23c
dkssud05 <hyeonuson51@gmail.com>
2025-11-10 18:17:56 +0900

    모드 설정

---
+++ play_mode.py
@@ -0,0 +1,12 @@
+from pico2d import *
+import game_framework
+import game_world
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
