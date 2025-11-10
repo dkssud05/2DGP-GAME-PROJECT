dd52e943efaf91e7daf43f8d2f80a3ed88b114ef
dkssud05 <hyeonuson51@gmail.com>
2025-11-10 18:23:53 +0900

    캐릭터 전역 변수로 리스트 추가

--- play_mode.py
+++ play_mode.py
@@ -5,6 +5,8 @@ from character1 import Character1
 from character2 import Character2
 from character3 import Character3
 
+characters = []
+
 def handle_events():
     events = get_events()
     for event in events:
