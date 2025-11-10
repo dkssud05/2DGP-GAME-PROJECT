13932177418e534fbf87f33cc64f7204d5b6c2ac
dkssud05 <hyeonuson51@gmail.com>
2025-11-10 18:22:20 +0900

    캐릭터 그리기 추가

--- play_mode.py
+++ play_mode.py
@@ -1,6 +1,9 @@
 from pico2d import *
 import game_framework
 import game_world
+from character1 import Character1
+from character2 import Character2
+from character3 import Character3
 
 def handle_events():
     events = get_events()
@@ -10,3 +13,25 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
 
+def init():
+    global characters
+
+    character1 = Character1()
+    character2 = Character2()
+    character3 = Character3()
+
+    game_world.add_object(character1, 1)
+    game_world.add_object(character2, 1)
+
+def update():
+    pass
+
+def draw():
+    global characters
+
+    clear_canvas()
+
+    for char in characters:
+        char.draw()
+
+    update_canvas()
