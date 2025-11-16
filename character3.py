07d0f8ab7d2e0912799566aa901a5874e0e652eb
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 21:59:59 +0900

    프레임에 따른 출력 오류 수정

--- character3.py
+++ character3.py
@@ -12,10 +12,10 @@ class Character3:
 
     TIME_PER_ACTION = 0.5
     ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-    FRAMES_PER_IDLE = 8
+    FRAMES_PER_IDLE = 10
     FRAMES_PER_RUN = 8
-    FRAMES_PER_JUMP = 2
-    FRAMES_PER_ATTACK = 6
+    FRAMES_PER_JUMP = 3
+    FRAMES_PER_ATTACK = 7
 
     def __init__(self):
         self.x, self.y = 500, 100
@@ -118,9 +118,9 @@ class Character3:
 
     def draw(self):
         if self.face_dir == 1:
-            self.image.clip_draw(int(self.frame) * 200, 0, 200, 200, self.x, self.y, 200, 200)
+            self.image.clip_draw(int(self.frame) * 126, 0, 126, 126, self.x, self.y, 200, 200)
         else:
-            self.image.clip_composite_draw(int(self.frame) * 200, 0, 200, 200, 0, 'h', self.x, self.y, 200, 200)
+            self.image.clip_composite_draw(int(self.frame) * 126, 0, 126, 126, 0, 'h', self.x, self.y, 200, 200)
 
     def handle_event(self, event):
         if self.player_id == 1:
