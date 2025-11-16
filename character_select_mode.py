5704b60a79fccc98eedc45a9de80458713d48018
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 16:45:41 +0900

    Step 3: Add title and character labels with font

--- character_select_mode.py
+++ character_select_mode.py
@@ -15,11 +15,17 @@ def init():
     char2_image = load_image('character2.motion/char2_Idle.png')
     char3_image = load_image('character3.motion/char3_Idle.png')
 
+    font = load_font(None, 32)
+
     highlighted = 1
     selected_character = None
 
 def finish():
-    pass
+    global char1_image, char2_image, char3_image, font
+    del char1_image
+    del char2_image
+    del char3_image
+    del font
 
 def update():
     pass
@@ -27,14 +33,20 @@ def update():
 def draw():
     clear_canvas()
 
+    # 타이틀 그리기
+    font.draw(250, 500, 'SELECT CHARACTER', (255, 255, 255))
+
     # 캐릭터 1 그리기 (왼쪽)
     char1_image.clip_draw(0, 0, 200, 200, 200, 300, 150, 150)
+    font.draw(130, 200, 'Character 1', (200, 200, 200))
 
     # 캐릭터 2 그리기 (중앙)
     char2_image.clip_draw(0, 0, 200, 200, 400, 300, 150, 150)
+    font.draw(330, 200, 'Character 2', (200, 200, 200))
 
     # 캐릭터 3 그리기 (오른쪽)
     char3_image.clip_draw(0, 0, 200, 200, 600, 300, 150, 150)
+    font.draw(530, 200, 'Character 3', (200, 200, 200))
 
     update_canvas()
 
