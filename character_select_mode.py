d3c4855f5f2edcdcf444bd5b1d1da12427be2b5f
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 16:37:23 +0900

    수정

--- character_select_mode.py
+++ character_select_mode.py
@@ -15,7 +15,6 @@ def draw():
     update_canvas()
 
 def handle_events():
-    global highlighted, selected_character
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
