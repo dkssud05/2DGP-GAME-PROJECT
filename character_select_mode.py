cf2c009f069ae3afb6633c9b5989d26c61a2886b
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 16:47:58 +0900

    캐릭터 선택 코드

--- character_select_mode.py
+++ character_select_mode.py
@@ -1,31 +1,27 @@
 from pico2d import *
 import game_framework
 
-selected_character = None  # 선택된 캐릭터 번호 (1, 2, 3)
-highlighted = 1  # 현재 하이라이트된 캐릭터
+selected_character = None
+highlighted = 1
 char1_image = None
 char2_image = None
 char3_image = None
-font = None
 
 def init():
-    global char1_image, char2_image, char3_image, font, highlighted, selected_character
+    global char1_image, char2_image, char3_image, highlighted, selected_character
 
     char1_image = load_image('character1.motion/char1_Idle.png')
     char2_image = load_image('character2.motion/char2_Idle.png')
     char3_image = load_image('character3.motion/char3_Idle.png')
 
-    font = load_font(None, 32)
-
     highlighted = 1
     selected_character = None
 
 def finish():
-    global char1_image, char2_image, char3_image, font
+    global char1_image, char2_image, char3_image
     del char1_image
     del char2_image
     del char3_image
-    del font
 
 def update():
     pass
@@ -33,20 +29,20 @@ def update():
 def draw():
     clear_canvas()
 
-    # 타이틀 그리기
-    font.draw(250, 500, 'SELECT CHARACTER', (255, 255, 255))
+    if highlighted == 1:
+        char1_image.clip_draw(0, 0, 200, 200, 200, 300, 180, 180)
+    else:
+        char1_image.clip_draw(0, 0, 200, 200, 200, 300, 150, 150)
 
-    # 캐릭터 1 그리기 (왼쪽)
-    char1_image.clip_draw(0, 0, 200, 200, 200, 300, 150, 150)
-    font.draw(130, 200, 'Character 1', (200, 200, 200))
+    if highlighted == 2:
+        char2_image.clip_draw(0, 0, 200, 200, 400, 300, 180, 180)
+    else:
+        char2_image.clip_draw(0, 0, 200, 200, 400, 300, 150, 150)
 
-    # 캐릭터 2 그리기 (중앙)
-    char2_image.clip_draw(0, 0, 200, 200, 400, 300, 150, 150)
-    font.draw(330, 200, 'Character 2', (200, 200, 200))
-
-    # 캐릭터 3 그리기 (오른쪽)
-    char3_image.clip_draw(0, 0, 200, 200, 600, 300, 150, 150)
-    font.draw(530, 200, 'Character 3', (200, 200, 200))
+    if highlighted == 3:
+        char3_image.clip_draw(0, 0, 200, 200, 600, 300, 180, 180)
+    else:
+        char3_image.clip_draw(0, 0, 200, 200, 600, 300, 150, 150)
 
     update_canvas()
 
