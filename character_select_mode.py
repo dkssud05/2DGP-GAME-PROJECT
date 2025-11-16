621004ad69f512d13c9a61d56b7abf915603729b
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 16:37:14 +0900

    캐릭터 픽창 추가

---
+++ character_select_mode.py
@@ -0,0 +1,32 @@
+from pico2d import *
+import game_framework
+
+def init():
+    pass
+
+def finish():
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+
+    update_canvas()
+
+def handle_events():
+    global highlighted, selected_character
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
