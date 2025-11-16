1821bdeb7d35eceac1df6f89f34635ba64641827
dkssud05 <hyeonuson51@gmail.com>
2025-11-16 16:41:53 +0900

    Step 1: Add basic variables and image loading for character selection

--- character_select_mode.py
+++ character_select_mode.py
@@ -1,8 +1,25 @@
 from pico2d import *
 import game_framework
 
+# 전역 변수
+selected_character = None  # 선택된 캐릭터 번호 (1, 2, 3)
+highlighted = 1  # 현재 하이라이트된 캐릭터
+char1_image = None
+char2_image = None
+char3_image = None
+font = None
+
 def init():
-    pass
+    global char1_image, char2_image, char3_image, font, highlighted, selected_character
+
+    char1_image = load_image('character1.motion/char1_Idle.png')
+    char2_image = load_image('character2.motion/char2_Idle.png')
+    char3_image = load_image('character3.motion/char3_Idle.png')
+
+    font = load_font()
+
+    highlighted = 1
+    selected_character = None
 
 def finish():
     pass
