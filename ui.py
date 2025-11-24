f81d15ba59b842ec6ebd09cdec790293e76d01a2
dkssud05 <hyeonuson51@gmail.com>
2025-11-24 12:44:05 +0900

    캐릭터 체력바 추가

--- ui.py
+++ ui.py
@@ -1,11 +1,23 @@
 from pico2d import *
 
 class UI:
-    def __init__(self):
-        pass
+    def __init__(self, max_hp = 200):
+        self.max_hp = max_hp
+        self.current_hp = max_hp
 
-    def update(self):
-        pass
+        self.x = 50
+        self.y = 550
+        self.width = 300
+        self.height = 20
+
+    def update(self, hp):
+        self.current_hp = max(0, min(hp, self.max_hp))
 
     def draw(self):
-        pass
+        draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height)
+
+        hp_ratio = self.current_hp / self.max_hp
+        hp_width = self.width * hp_ratio
+
+        if hp_width > 0:
+            draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height)
