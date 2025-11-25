b18e4c91ab9526d8902006ad89210993dd468dc8
dkssud05 <hyeonuson51@gmail.com>
2025-11-25 19:04:14 +0900

    화면에 체력바 UI 안에 채우고 테두리 설정

--- ui.py
+++ ui.py
@@ -14,10 +14,10 @@ class UI:
         self.current_hp = max(0, min(hp, self.max_hp))
 
     def draw(self):
-        draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height)
+        draw_rectangle(self.x-1, self.y-1, self.x + self.width+1, self.y + self.height+1, 0, 0, 0, filled=False)
 
         hp_ratio = self.current_hp / self.max_hp
         hp_width = self.width * hp_ratio
 
         if hp_width > 0:
-            draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height)
+            draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, 255, 0, 0, filled=True)
